746fe30256e5c33bff2fdb92b8f2a131fac50621
Yuchen Huang <oocasthy@umich.edu>
2017-04-17 17:47:45 -0400

    depart from garage

--- Patrol/patrol.py
+++ Patrol/patrol.py
@@ -79,6 +79,10 @@ class Patrol:
 
         await robot.set_lift_height(1.0).wait_for_completed()
         await robot.set_head_angle(degrees(30)).wait_for_completed()
+
+        # TODO: this distance is likely to be inaccurate
+        await robot.drive_straight(distance_mm(100)).wait_for_completed()
+        await robot.turn_in_place(degrees(-90))
         
         # await self.loop(robot)
         await self.loopPath(robot)
